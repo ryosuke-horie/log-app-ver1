4713b1da46c2f9f7ae3a05e5ad24a6a598284545
ryosuke-horie <ryosuke03140@gmial.com>
2023-04-11 09:53:43 +0900

    フォームを作成し、CSSでスタイリング

--- src/components/MoneyInputForm.tsx
+++ src/components/MoneyInputForm.tsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React from 'react';
+import styles from './MoneyInputForm.module.css';
 
-const MoneyInputForm = () => {
+const MoneyInputForm: React.FC = () => {
+  const ClickButton = (): void => {
+    alert('登録しました');
+
+    // TODO: ここで入力された値を取得する
+  };
+
   return (
-    <>
-      <h1>金銭ログ</h1>
+    <div className={styles["form-container"]}>
+      <h1 className={styles["form-title"]}>金銭ログ</h1>
       <form>
-        <div>
-          <label>日付</label>
-          <input type="date" />
+        <div className={styles["form-group"]}>
+          <label className={styles["form-label"]}>日付</label>
+          <input type="date" className={styles["input-field"]} />
         </div>
-        <div>
-          <label>金額</label>
-          <input type="number" />
+        <div className={styles["form-group"]}>
+          <label className={styles["form-label"]}>金額</label>
+          <input type="number" className={styles["input-field"]} />
         </div>
-        <div>
-          <label>カテゴリ</label>
-          <select>
+        <div className={styles["form-group"]}>
+          <label className={styles["form-label"]}>カテゴリ</label>
+          <select className={styles["input-field"]}>
             <option value="食費">食費</option>
             <option value="交通費">交通費</option>
             <option value="日用品">日用品</option>
@@ -23,9 +30,12 @@ const MoneyInputForm = () => {
             <option value="その他">その他</option>
           </select>
         </div>
+        <button className={styles["submit-button"]} onClick={ClickButton}>
+          登録
+        </button>
       </form>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default MoneyInputForm
+export default MoneyInputForm;
