70d42129feb3b91db1fa0c505a03eb2b292be2b9
fsworks\r-horie <r-horie@mamiya-its.co.jp>
2023-04-12 12:52:25 +0900

    ヘッダー作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css'
+import Header from './components/Header'
 import ExpenseForm from './components/ExpenseForm/index'
 import ExpenseList from './components/ExpenseList/index'
 
 function App() {
   return (
     <div className="App">
-      <h1>MyLogApp Ver.1</h1>
+      <Header />
       <ExpenseForm />
       <ExpenseList />
     </div>
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header: React.FC = () => {
+  return (
+    <>
+      <h1>MyLogApp Ver.1</h1>
+      <div className="menus">
+        <div className="menu">Home</div>
+        <div className="menu">お金関係</div>
+      </div>
+    </>
+  )
+}
+
+export default Header
