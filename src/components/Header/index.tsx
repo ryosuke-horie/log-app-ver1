d35b3789f6f8c9602af748bf72a2a8de8ec3818f
ryosuke-horie <ryosuke03140@gmial.com>
2023-04-12 20:22:44 +0900

    ヘッダーにメニューをモーダルで実装

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,82 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { FaBars } from 'react-icons/fa';
+import Modal from 'react-modal';
 
-// TODO 意味ないので変更
-const Wrapper = styled.div`
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  background-color: #282c34;
+  color: #fff;
+`;
+
+const Logo = styled.h1`
+  font-size: 24px;
+  font-weight: bold;
+  margin: 0;
+`;
+
+const MenuIcon = styled(FaBars)`
+  display: block;
+  font-size: 24px;
+  color: #fff;
+  cursor: pointer;
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+const CustomModal = styled(Modal)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: rgba(0, 0, 0, 0.7);
+  border-radius: 5px;
+  outline: none;
+  padding: 20px;
+  color: #fff;
   text-align: center;
-`
+  height: 700px;
+  width: 300px;
+
+  @media (min-width: 768px) {
+    width: 400px;
+  }
+`;
 
 const Header: React.FC = () => {
-  return (
-    <Wrapper>
-      <h1>MyLogApp Ver.1</h1>
-      <div className="menus">
-        <div className="menu">Home</div>
-        <div className="menu">お金関係</div>
-      </div>
-    </Wrapper>
-  )
-}
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
-export default Header
+  const handleOpenModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalIsOpen(false);
+  };
+
+  return (
+    <HeaderContainer>
+      <Logo>MyLogApp Ver.1</Logo>
+      <div>
+        <MenuIcon onClick={handleOpenModal} />
+        <CustomModal
+          isOpen={modalIsOpen}
+          onRequestClose={handleCloseModal}
+          ariaHideApp={false}
+        >
+          <h2>メニュー</h2>
+          <div>Home</div>
+          <div>お金関係</div>
+          <button onClick={handleCloseModal}>閉じる</button>
+        </CustomModal>
+      </div>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
