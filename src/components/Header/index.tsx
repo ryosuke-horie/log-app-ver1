0004d600284826d2f595207f4ab2994079899858
fsworks\r-horie <r-horie@mamiya-its.co.jp>
2023-04-12 17:22:30 +0900

    メニューのUIを調整

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,45 @@
 import React from 'react'
 import styled from 'styled-components'
 
-// TODO 意味ないので変更
-const Wrapper = styled.div`
-  text-align: center;
-`
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  width: 100%;
+  max-width: 800px;
+`;
 
-const Header: React.FC = () => {
+const Title = styled.h1`
+  font-size: 24px;
+  margin: 0;
+`;
+
+const Menus = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const MenuItem = styled.div`
+  margin: 0 10px;
+`;
+
+const MyLogApp: React.FC = () => {
   return (
-    <Wrapper>
-      <h1>MyLogApp Ver.1</h1>
-      <div className="menus">
-        <div className="menu">Home</div>
-        <div className="menu">お金関係</div>
-      </div>
-    </Wrapper>
+    <Container>
+      <Title>MyLogApp Ver.1</Title>
+      <Menus>
+        <MenuItem>Home</MenuItem>
+        <MenuItem>Expense</MenuItem>
+        <MenuItem>Study</MenuItem>
+        <MenuItem>ToDo</MenuItem>
+        <MenuItem>Interest</MenuItem>
+        <MenuItem>Training</MenuItem>
+      </Menus>
+    </Container>
   )
 }
 
-export default Header
+export default MyLogApp
