05539c1425c25dbe42fd5d182e3ff1b3ab7f3db5
fsworks\r-horie <r-horie@mamiya-its.co.jp>
2023-04-12 12:52:37 +0900

    npm run fix

--- src/components/ExpenseList/handleDelete.ts
+++ src/components/ExpenseList/handleDelete.ts
@@ -2,6 +2,6 @@ import { ref, remove } from 'firebase/database'
 import { database } from '../../firebase'
 
 export const handleDelete = (id: string) => {
-    const expenseRef = ref(database, `expenses/${id}`)
-    remove(expenseRef)
+  const expenseRef = ref(database, `expenses/${id}`)
+  remove(expenseRef)
 }
