97643b782bddd659f406188599b6b1581785f46b
ryosuke-horie <ryosuke03140@gmial.com>
2023-04-18 08:48:48 +0900

    npm fix

--- src/atoms/Logo/index.stories.tsx
+++ src/atoms/Logo/index.stories.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import Logo from './index';
+import React from 'react'
+import Logo from './index'
 
 export default {
   title: 'Atoms/Logo',
   component: Logo,
-};
+}
 
-export const Default = () => <Logo />;
+export const Default = () => <Logo />
--- src/atoms/Logo/index.tsx
+++ src/atoms/Logo/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
 const LogoContainer = styled.h1`
   font-size: 24px;
   font-weight: bold;
   margin: 0;
-`;
+`
 
 const Logo: React.FC = () => {
-  return <LogoContainer>MyLogApp Ver.1</LogoContainer>;
-};
+  return <LogoContainer>MyLogApp Ver.1</LogoContainer>
+}
 
-export default Logo;
+export default Logo
