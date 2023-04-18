b030cf22e1664f8f4cc2ca2ad07c7f965906657b
fsworks\r-horie <r-horie@mamiya-its.co.jp>
2023-04-18 16:57:08 +0900

    コメント追加

--- src/atoms/MenuIcon/index.tsx
+++ src/atoms/MenuIcon/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { FaBars } from 'react-icons/fa'
 
+// スタイル定義
 const MenuIconStyle = styled(FaBars)`
   display: block;
   font-size: 24px;
@@ -13,11 +14,13 @@ const MenuIconStyle = styled(FaBars)`
   }
 `
 
+// Propsの型定義
 interface MenuIconProps {
   role: string
   onClick: () => void
 }
 
+// コンポーネント定義
 const MenuIcon = ({ role, onClick }: MenuIconProps) => {
   return <MenuIconStyle role={role} onClick={onClick}></MenuIconStyle>
 }
