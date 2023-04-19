5f707338d23579c464e25ea7f487100d5d74cd20
ryosuke-horie <ryosuke03140@gmial.com>
2023-04-20 01:14:11 +0900

    routerの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ExpensePage from './pages/Expense'
 
 function App() {
   return (
-    <div className="App">
-      <ExpensePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route index path="/" element={<ExpensePage />} />
+      </Routes>
+    </Router>
   )
 }
 
