a634732bd4f87365b9bbb5f1f3153b37baa21a87
ryosuke-horie <ryosuke03140@gmial.com>
2023-04-28 13:28:12 +0900

    weightページにヘッダーを追加

--- src/pages/Weight/index.tsx
+++ src/pages/Weight/index.tsx
@@ -1,10 +1,16 @@
 import React from "react"
+import Header from "../../molecules/Header"
 import WeightForm from "../../components/WeightForm"
 import WeightList from "../../components/WeightList"
+
+const handleMenuClose = () => {
+    console.log('Menu closed!')
+}  
 
 const WeightPage = () => {
     return (
         <>
+        <Header onMenuClose={handleMenuClose} />
         <WeightForm />
         <WeightList />
         </>
